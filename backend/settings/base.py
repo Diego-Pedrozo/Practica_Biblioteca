7d5ea703b5d1ab0f28b17c213d0849d96ea2abc3
Diego Pedrozo <62295810+Diego-Pedrozo@users.noreply.github.com>
2024-06-15 20:09:09 -0500

    Update base.py

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -224,6 +224,7 @@ CORS_ALLOW_HEADERS = [
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:5173',
 ]
+CSRF_TRUSTED_ORIGINS = ['https://practica-biblioteca-back.onrender.com']
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
